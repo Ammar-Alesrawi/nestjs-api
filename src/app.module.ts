93690579ef245fe86bf3e5053138071e862135b4
Ammar <esrawiaammar22@gmail.com>
2024-07-16 18:47:55 +0300

    add prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,5 +2,6 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
-@Module({ imports: [AuthModule, UserModule, BookmarkModule] })
+import { PrismaModule } from './prisma/prisma.module';
+@Module({ imports: [AuthModule, UserModule, BookmarkModule, PrismaModule] })
 export class AppModule {}
